694a96b3880d0fe2405e63453ddfe10b39abba53
Francisco <francisco.vera.95@gmail.com>
2025-12-09 16:42:01 -0600

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import NavBar from './components/Navbar'
+import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import AboutMe from './components/AboutMe'
 import Carousel from './components/Carousel'
